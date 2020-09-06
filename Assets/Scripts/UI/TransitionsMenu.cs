eab8556d2a28e0ce43c6098a40e42839ca21d3d6
crazy78cyxapuk <olezhki.net@gmail.com>
2020-09-06 18:00:34 +0300

    сделан прототип меню

--- Assets/Scripts/UI/TransitionsMenu.cs
+++ Assets/Scripts/UI/TransitionsMenu.cs
@@ -5,8 +5,66 @@ using UnityEngine.SceneManagement;
 
 public class TransitionsMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject MainScreen, LevelsScreen, LevelsChoiceScreen, SettingsScreen, AboutScreen;
+    private GameObject LastScreen;
+
     public void TransitionScene(string scene)
     {
         SceneManager.LoadScene(scene);
     }
+
+
+
+    #region Buttons in scene "Menu"
+
+    public void MainToLevels()
+    {
+        LevelsScreen.SetActive(true);
+        MainScreen.SetActive(false);
+    }
+
+    public void LevelsToChoiceLevel()
+    {
+        LevelsChoiceScreen.SetActive(true);
+        LevelsScreen.SetActive(false);
+    }
+
+    public void HideSettingsScreen()
+    {
+        SettingsScreen.SetActive(false);
+        LastScreen.SetActive(true);
+    }
+
+    public void ShowSettingsScreen(GameObject obj)
+    {
+        LastScreen = obj;
+        LastScreen.SetActive(false);
+        SettingsScreen.SetActive(true);
+    }
+
+    public void HideAboutScreen()
+    {
+        SettingsScreen.SetActive(true);
+        AboutScreen.SetActive(false);
+    }
+
+    public void ShowAboutScreen()
+    {
+        AboutScreen.SetActive(true);
+        SettingsScreen.SetActive(false);
+    }
+
+    public void HideChoiceLevelsScreen()
+    {
+        LevelsScreen.SetActive(true);
+        LevelsChoiceScreen.SetActive(false);
+    }
+
+    public void ShowChoiceLevelsScreen()
+    {
+        LevelsChoiceScreen.SetActive(true);
+        LevelsScreen.SetActive(false);
+    }
+
+    #endregion
 }
