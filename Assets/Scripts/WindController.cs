6e4912a1739e895f417a275ae830ccbdd0f12b55
crazy78cyxapuk <olezhki.net@gmail.com>
2020-09-19 10:32:06 +0300

    сделан лесной биом

---
+++ Assets/Scripts/WindController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindController : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Vector2 position = transform.position;
+        //Vector2 targetPosition = collision.transform.position;
+        //Vector2 direction = targetPosition - position;
+        //direction.Normalize();
+        //int moveSpeed = 10;
+        //collision.transform.position += (Vector2)(direction * moveSpeed * Time.deltaTime);
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 500 * Time.deltaTime);
+        }
+    }
+}
